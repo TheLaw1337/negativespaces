41d20a96c5879705a81d114fa4a51ffae31c4730
Martin Braun <martin.braun.1991@live.com>
2018-02-26 05:00:06 +0100

    Fixed the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,29 @@
 let exec = require("child_process").exec;
 let fs = require("fs")
-var path = require('path');
 let path = require("path");
 let process = require("process");
 let mkdirp = require("mkdirp");
 let zipFolder = require("zip-folder");
 let gulp = require("gulp");
-
 let conf = require("./gulpconf");
 
 let runSequence = require("run-sequence");
-let clean = require("gulp-clean");
-
 var size = require("gulp-size");
 let plumber = require("gulp-plumber");
 let func = require("gulp-function");
 let clean = require("gulp-clean");
-let plumber = require("gulp-plumber");
-let foreach = require("gulp-foreach");
 let deleteFile = require("gulp-delete-file");
 let gulpSrcFiles= require("gulp-src-files");
 
 let luaminify = require('gulp-luaminify');
 let spritesmith = require('gulp.spritesmith');
 
-gulp.task("clean-before", function() {
 let win = process.platform === "win32";
 let mac = process.platform === "darwin";
 
 gulp.task("delete-dist", function () {
 	return gulp.src("dist", { read: false }).pipe(clean());
 });
- 
 
 gulp.task("dist", ["delete-dist"], function () {
 	return gulp.src(["./src/**/*.{" + conf["srcExt"] + "}"])
@@ -41,75 +33,50 @@ gulp.task("dist", ["delete-dist"], function () {
 });
 
 gulp.task('minify-lua', function () {
-		return gulp.src(["src/**/*.lua"])
-		.pipe(plumber())
 	return gulp.src(["./dist/src/**/*.lua"], { base: "." })
 	.pipe(plumber())
 		.pipe(size({ showFiles: true }))
 		.pipe(luaminify())
-		.pipe(gulp.dest("dist/src"));
 		.pipe(gulp.dest("."));
 });
 
 var animations;
-gulp.task("reg-animations", function () {	
 gulp.task("reg-animations", function () {
 	animations = [];
-	return gulp.src(["src/**/*.ani.png"], { read: false })
-		.pipe(foreach(function(stream, file) {
 	return gulp.src(["./src/**/*.ani.png"], { read: false })
 		.pipe(size({ showFiles: true }))
 		.pipe(func.forEach(function (file, enc) {
 			let d = path.dirname(file.path).replace(__dirname + "/", "");
 			let g = path.win32.basename(file.path).split(/_/)[0];
-			if(animations.length < 1 || animations.indexOf([d,g]) < 0) {
-				animations.push([d,g]);
 			if (animations.length < 1 || animations.indexOf([d, g]) < 0) {
 				animations.push([d, g]);
-
 			}
-			return stream
 			return enc
 		}));
 });
 
-gulp.task("merge-animations", ["reg-animations"], function () {	
 gulp.task("merge-animations", ["reg-animations"], function () {
 	var subTasks = [];
 	animations.forEach(arr => {
-		subTasks.push(new Promise(function(resolve, reject) { 
 		subTasks.push(new Promise(function (resolve, reject) {
 			gulp.src([arr[0] + "/" + arr[1] + "_*.ani.png"])
     		.pipe(size({ showFiles: true }))
 				.pipe(spritesmith({
 					imgName: arr[1] + ".a.png",
-					cssName: "dummy.css",
 					cssName: "dummy.ani.css",
 					algorithm: "top-down",
-					algorithmOpts: { 
 					algorithmOpts: {
 						sort: false
 					}
 				}))
-        .on('error', reject)
-				.pipe(gulp.dest("dist/" + arr[0]))
 				.on('error', reject)
 				.pipe(gulp.dest(arr[0]))
-        .on('end', resolve);
-			}))
 				.pipe(gulp.dest("./dist/" + arr[0]))
 				.on('end', resolve);
 		}));
 	});
-	return Promise.all(subTasks);
 	return Promise.all(subTasks).then();
 });
-
-gulp.task("clean-after", function () {
-	return Promise.all([
-		gulp.src("dist/src/**/dummy.css", { read: false }).pipe(clean()),
-		gulp.src("src/**/dummy.css", { read: false }).pipe(clean())
-	]);
 
 gulp.task("clean", function() {
 	return gulp.src("./dist/src/**/*.ani.{png,css}", { read: false })
@@ -132,13 +99,6 @@ gulp.task("make-love", ["clean"], function () {
 		});
 	});
 });
-
-gulp.task("default", ["build"], function() {
-	gulp.watch("src/**/*.lua", ["minify-lua"]);
-	gulp.watch("src/**/*.ani.png", ["build"]);
-})
-
-
 
 gulp.task("make-win", function () {
 	let loveDependencyExt = "*.{exe,dll}"
@@ -165,9 +125,6 @@ gulp.task("make-win", function () {
 		}));
 });
 
-gulp.task("build", function() {
-	runSequence("clean-before", ["minify-lua", "merge-animations"], "clean-after", function() {
-		console.log("Build completed!");
 gulp.task("make-android", function () {
 
 });
@@ -180,41 +137,7 @@ gulp.task("make-mac", function () {
 				throw new Error("Mac build is not implemented as of now.");
 			}
 		});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	});
-
-
 });
 
 gulp.task("make-ios", ["make-love"], function () {
