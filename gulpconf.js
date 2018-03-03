ec9a2f7eb6fbc7d7a6a6a7a83bef2082e7169602
Daniel Sobczak <20947896+TheLaw1337@users.noreply.github.com>
2018-03-03 13:51:36 +0100

    set launching

--- gulpconf.js
+++ gulpconf.js
@@ -4,19 +4,19 @@ module.exports = {
 	"minifyExclude": "**/lib/**/*", // any files within a lib folder won't be minified, because not all are luamin friendly (path will get the prefix "!./dist/src/")
 
 	"makeWin": false, // will create build exe on default build (no final SFX compression, feel free to pull request)
-	"loveWinDir": "/Users/marty/love-win32", // contains love.exe and dll-files
+	"loveWinDir": "/Users/user/love-win32", // contains love.exe and dll-files
 
 	"makeAndroid": false, // will copy .love file and injection files into the directory below and will run gradlew build on default build
-	"loveAndroidDir": "/Users/marty/_my_/AS/love2d-admob-inappbilling-gameservices-android", // injection files will work with with the AdMob+InAppBilling-GameServices fork - not available yet
-
+	"loveAndroidDir": "/Users/user/_my_/AS/love2d-admob-inappbilling-gameservices-android", // injection files will work with with the AdMob+InAppBilling-GameServices fork - not available yet
+	
 	"makeMac": false, // won't work / not supported (sry, feel free to pull request)
 	"loveMacDir": "/Applications",
 
 	"makeiOS": false, // will copy .love file and injection files into the directory below on default build
-	"loveiOSDir": "/Users/marty/_my_/XC/love2d-admob-inapppurchases-gamecenter-ios", // injection files will work with with the AdMob+InAppPurchases-GameCenter fork - not available yet
+	"loveiOSDir": "/Users/user/_my_/XC/love2d-admob-inapppurchases-gamecenter-ios", // injection files will work with with the AdMob+InAppPurchases-GameCenter fork - not available yet
 
 	"windows" : { // all paths above for Windows systems
-		"loveWinDir": "C:\\Program Files (x86)\\LOVE",
+		"loveWinDir": "C:\\Program Files\\LOVE",
 		"loveAndroidDir": "Z:\\_my_\\AS\\love2d-admob-inappbilling-gameservices-android",
 		"loveiOSDir": "Z:\\_my_\\XC\\love2d-admob-inapppurchases-gamecenter-ios",
 	}
