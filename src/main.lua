f498ed4c9b7e889145e39fd5365540dda27096a4
Daniel Sobczak <20947896+TheLaw1337@users.noreply.github.com>
2018-03-28 18:02:30 +0200

    players can move now

--- src/main.lua
+++ src/main.lua
@@ -10,11 +10,18 @@ function love.load(args)
 
 	local tileset_width = tileset:getWidth()
 	local tileset_height = tileset:getHeight()
+	player_width = black:getWidth()
+	player_height = black:getHeight()
 
 	love.graphics.setBackgroundColor(102, 153, 153)
 
 	width = tileset_width / 2
 	height = tileset_height / 2
+
+	p1_x = 1
+	p1_y = 1
+	p2_x = 20
+	p2_y = 11
 
 	-- 8,8 160,88
 
@@ -54,6 +61,29 @@ function love.load(args)
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1}
 		
 	}
+	print(player_width)
+end
+
+function love.keypressed(key)
+	if key == "a" then
+		p1_x = p1_x - 1
+	elseif key == "d" then
+		p1_x = p1_x + 1
+	elseif key == "w" then
+		p1_y = p1_y - 1
+	elseif key == "s" then
+		p1_y = p1_y + 1
+	end
+
+	if key == "left" then
+		p2_x = p2_x - 1
+	elseif key == "right" then
+		p2_x = p2_x + 1
+	elseif key == "up" then
+		p2_y = p2_y - 1
+	elseif key == "down" then
+		p2_y = p2_y + 1
+	end
 end
 
 function love.draw()
@@ -73,6 +103,6 @@ function love.draw()
 		end
 	end
 
-	love.graphics.draw(white, 8, 8)
-	love.graphics.draw(black, 160, 88)
+	love.graphics.draw(white, player_width * p1_x, player_height * p1_y)
+	love.graphics.draw(black, player_width * p2_x, player_height * p2_y)
 end
