35076e936b364ae09bb13fb1497feea1743e0e21
Daniel Sobczak <20947896+TheLaw1337@users.noreply.github.com>
2018-06-08 17:00:51 +0200

    simple start screen added

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,6 @@
 function love.load(args)
+	gamestate = "menu"
+	
 	love.graphics.setDefaultFilter('nearest', 'nearest')
 	tileset = love.graphics.newImage("map_tileset.png")
 	black = love.graphics.newImage("black.png")
@@ -6,6 +8,7 @@ function love.load(args)
 	font = love.graphics.newFont("zx_spectrum-7.ttf", 64)
 	font:setFilter('nearest', 'nearest')
 	love.graphics.setFont(font)
+	scr_width = love.graphics.getWidth()
 	
 
 	local tileset_width = tileset:getWidth()
@@ -60,88 +63,66 @@ function love.load(args)
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1}
 		
+		
 	}
-	
+
 end
 
 function love.keypressed(key)
-	--[[local nextX = p1_x
-	local nextY = p1_y
+	if gamestate == "menu" then
+		if key == "return" then
+			gamestate = "game"
+		end
+	end
 	
-	if (key == "a") and p1_x > 1 then 
-		nextX = nextX - 1
-		--print(p1_x, p1_y)
-		fow()
-	elseif key == "d" and p1_x < 20 then
-		--print(p1_x, p1_y)
-		nextX = nextX + 1
-		fow()
-	elseif (key == "w") and p1_y > 1 then
-		nextY = nextY - 1
-		--print(p1_x, p1_y)
-		fow()
-	elseif key == "s" and p1_y < 11 then
-		nextY = nextY + 1
-		--print(p1_x, p1_y)
-		fow()
-	end
+	if gamestate == "game" then
+		local nextX = p1_x
+		local nextY = p1_y
 
-	if tilemap[nextY][nextX] ~=2 then
-		p1_x = nextX
-		p1_y = nextY
-
-		if tilemap[p1_y][p1_x] == 3 then
-			print("You win!")
+		if key == "w" and p1_y > 1 then
+			nextY = nextY - 1
+		elseif key == "s" and p1_y < 11 then
+			nextY = nextY + 1 -- other keys!
+		elseif key == "a" and p1_x > 1 then
+			nextX = nextX - 1 -- other keys!
+		elseif key == "d" and p1_x < 20 then
+			nextX = nextX + 1 -- other keys!	
 		end
-	end
---]]
 
-	local nextX = p1_x
-	local nextY = p1_y
+		if tilemap[nextY][nextX] == 2 or tilemap[nextY][nextX] == 3 then -- not a wall, move the player
+			p1_x = nextX
+			p1_y = nextY
+			fow()
 
-	if key == "w" and p1_y > 1 then
-		nextY = nextY - 1
-	elseif key == "s" and p1_y < 11 then
-		nextY = nextY + 1 -- other keys!
-	elseif key == "a" and p1_x > 1 then
-		nextX = nextX - 1 -- other keys!
-	elseif key == "d" and p1_x < 20 then
-		nextX = nextX + 1 -- other keys!	
-	end
-
-	if tilemap[nextY][nextX] == 2 or tilemap[nextY][nextX] == 3 then -- not a wall, move the player
-		p1_x = nextX
-		p1_y = nextY
-		fow()
-
-		if tilemap[p1_y][p1_x] == 3 then --finish!
-			print("Player 1 - You win!")
+			if tilemap[p1_y][p1_x] == 3 then --finish!
+				print("You win")
+			end
 		end
-	end
 
-	local p2next_x = p2_x
-	local p2next_y = p2_y
+		local p2next_x = p2_x
+		local p2next_y = p2_y
 
-	if key == "left" and p2_x > 1 then
-		p2next_x = p2next_x - 1
-	elseif key == "right" and p2_x < 20 then
-		p2next_x = p2next_x + 1
-		print(p2_x, p2_y)
-	elseif key == "up" and p2_y > 1 then
-		p2next_y = p2next_y - 1
-		print(p2_x, p2_y)
-	elseif key == "down" and p2_y < 11 then
-		p2next_y = p2next_y + 1
-		print(p2_x, p2_y)
-	end
+		if key == "left" and p2_x > 1 then
+			p2next_x = p2next_x - 1
+		elseif key == "right" and p2_x < 20 then
+			p2next_x = p2next_x + 1
+			print(p2_x, p2_y)
+		elseif key == "up" and p2_y > 1 then
+			p2next_y = p2next_y - 1
+			print(p2_x, p2_y)
+		elseif key == "down" and p2_y < 11 then
+			p2next_y = p2next_y + 1
+			print(p2_x, p2_y)
+		end
 
-	if tilemap[p2next_y][p2next_x] == 1 or tilemap[p2next_y][p2next_x] == 3 then -- not a wall, move the player
-		p2_x = p2next_x
-		p2_y = p2next_y
-		fow()
+		if tilemap[p2next_y][p2next_x] == 1 or tilemap[p2next_y][p2next_x] == 3 then -- not a wall, move the player
+			p2_x = p2next_x
+			p2_y = p2next_y
+			fow()
 
-		if tilemap[p2_y][p2_x] == 3 then --finish!
-			print("Player 2 - You win!")
+			if tilemap[p2_y][p2_x] == 3 then --finish!
+				print("Player 2 wins")
+			end
 		end
 	end
 end
@@ -172,22 +153,47 @@ end
 
 
 function love.draw()
-	love.graphics.push("all")
-	love.graphics.setColor(0, 0, 0)
-	love.graphics.print("NEGATIVE SPACES", 32, 400)
-	love.graphics.pop()
+	if gamestate == "menu" then
+		love.graphics.push("all")
+		love.graphics.setColor(0, 0, 0)
+		love.graphics.printf("NEGATIVE SPACES", 0, 400, scr_width, "center")
+		love.graphics.printf( "PRESS START", 0, 500, scr_width, "center")
+		love.graphics.pop()
 
-	love.graphics.scale(4, 4)
-	for i,row in ipairs(tilemap) do  
-		for j,tile in ipairs(row) do 
-				local v = tilemap[i][j] + 1
-				if fogofwar[i][j] == 1 then
-					love.graphics.draw(tileset, quads[v], j * width, i * height)
-				end
+		love.graphics.scale(4, 4)
+		for i,row in ipairs(tilemap) do  
+			for j,tile in ipairs(row) do 
+					local v = tilemap[i][j] + 1
+					if fogofwar[i][j] == 1 then
+						love.graphics.draw(tileset, quads[v], j * width, i * height)
+					end
+			end
 		end
+
+		
+		love.graphics.draw(white, player_width * p1_x, player_height * p1_y)
+		love.graphics.draw(black, player_width * p2_x, player_height * p2_y)
 	end
 
+	if gamestate == "game" then
+			love.graphics.push("all")
+			love.graphics.setColor(0, 0, 0)
+			
+			
+			love.graphics.pop()
 	
-	love.graphics.draw(white, player_width * p1_x, player_height * p1_y)
-	love.graphics.draw(black, player_width * p2_x, player_height * p2_y)
+			love.graphics.scale(4, 4)
+			for i,row in ipairs(tilemap) do  
+				for j,tile in ipairs(row) do 
+						local v = tilemap[i][j] + 1
+						if fogofwar[i][j] == 1 then
+							love.graphics.draw(tileset, quads[v], j * width, i * height)
+						end
+				end
+			end
+	
+			
+			love.graphics.draw(white, player_width * p1_x, player_height * p1_y)
+			love.graphics.draw(black, player_width * p2_x, player_height * p2_y)
+	end
 end
