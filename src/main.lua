e0ef6967bd2f8249a60688e44485db59ce00338d
Daniel Sobczak <20947896+TheLaw1337@users.noreply.github.com>
2018-06-09 18:07:53 +0200

    hump.gamestate implemented

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,141 @@
+Gamestate = require "gamestate"
+
+menu = {}
+game = {}
+roll = {}
+function menu:keypressed(key, code)
+	if key == "return" then
+		Gamestate.switch(game)
+	end
+end
+
+function menu:draw()
+	love.graphics.push("all")
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.printf("NEGATIVE SPACES", 0, 400, scr_width, "center")
+	love.graphics.printf( "PRESS START BUTTON", 0, 500, scr_width, "center")
+	love.graphics.pop()
+
+	love.graphics.scale(4, 4)
+	for i,row in ipairs(tilemap) do  
+		for j,tile in ipairs(row) do 
+				local v = tilemap[i][j] + 1
+				if fogofwar[i][j] == 1 then
+					love.graphics.draw(tileset, quads[v], j * width, i * height)
+				end
+		end
+	end
+
+	
+	love.graphics.draw(white, player_width * p1_x, player_height * p1_y)
+	love.graphics.draw(black, player_width * p2_x, player_height * p2_y)
+end
+
+function game:keypressed(key, code)
+	if key == "return" then
+		Gamestate.switch(roll)
+	end
+
+	local nextX = p1_x
+	local nextY = p1_y
+
+	if key == "w" and p1_y > 1 then
+		nextY = nextY - 1
+	elseif key == "s" and p1_y < 11 then
+		nextY = nextY + 1 -- other keys!
+	elseif key == "a" and p1_x > 1 then
+		nextX = nextX - 1 -- other keys!
+	elseif key == "d" and p1_x < 20 then
+		nextX = nextX + 1 -- other keys!	
+	end
+
+	if tilemap[nextY][nextX] == 2 or tilemap[nextY][nextX] == 3 then -- not a wall, move the player
+		p1_x = nextX
+		p1_y = nextY
+		fow()
+
+		if tilemap[p1_y][p1_x] == 3 then --finish!
+			print("Player 1 - You win!")
+		end
+	end
+
+	local p2next_x = p2_x
+	local p2next_y = p2_y
+
+	if key == "left" and p2_x > 1 then
+		p2next_x = p2next_x - 1
+	elseif key == "right" and p2_x < 20 then
+		p2next_x = p2next_x + 1
+		print(p2_x, p2_y)
+	elseif key == "up" and p2_y > 1 then
+		p2next_y = p2next_y - 1
+		print(p2_x, p2_y)
+	elseif key == "down" and p2_y < 11 then
+		p2next_y = p2next_y + 1
+		print(p2_x, p2_y)
+	end
+
+	if tilemap[p2next_y][p2next_x] == 1 or tilemap[p2next_y][p2next_x] == 3 then -- not a wall, move the player
+		p2_x = p2next_x
+		p2_y = p2next_y
+		fow()
+
+		if tilemap[p2_y][p2_x] == 3 then --finish!
+			print("Player 2 - You win!")
+		end
+	end
+end
+
+function game:draw()
+		love.graphics.push("all")
+		love.graphics.setColor(0, 0, 0)
+
+		love.graphics.printf("PLAYER " .. current_player .. " - YOUR TURN", 0, 400, scr_width, "center")
+		love.graphics.printf("PRESS ENTER TO ROLL A DICE", 0, 430, scr_width, "center")
+		love.graphics.pop()
+
+		love.graphics.scale(4, 4)
+		for i,row in ipairs(tilemap) do  
+			for j,tile in ipairs(row) do 
+					local v = tilemap[i][j] + 1
+					if fogofwar[i][j] == 1 then
+						love.graphics.draw(tileset, quads[v], j * width, i * height)
+					end
+			end
+		end
+
+		
+		love.graphics.draw(white, player_width * p1_x, player_height * p1_y)
+		love.graphics.draw(black, player_width * p2_x, player_height * p2_y)
+	end
+
+function roll:draw()
+	love.graphics.push("all")
+		love.graphics.setColor(0, 0, 0)
+
+		love.graphics.printf("ROLL A DICE!", 0, 400, scr_width, "center")
+		love.graphics.pop()
+
+		love.graphics.scale(4, 4)
+		for i,row in ipairs(tilemap) do  
+			for j,tile in ipairs(row) do 
+					local v = tilemap[i][j] + 1
+					if fogofwar[i][j] == 1 then
+						love.graphics.draw(tileset, quads[v], j * width, i * height)
+					end
+			end
+		end
+
+		
+		love.graphics.draw(white, player_width * p1_x, player_height * p1_y)
+		love.graphics.draw(black, player_width * p2_x, player_height * p2_y)
+	end
+
+function love.update(dt)
+	Gamestate:update(dt)
+end
+
 function love.load(args)
-	gamestate = "menu"
 	current_player = 1
 	
 	love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -66,70 +202,12 @@ function love.load(args)
 		
 		
 	}
-
+	
+	Gamestate.switch(menu)
 end
 
 function love.keypressed(key)
-	if gamestate == "menu" then
-		if key == "return" then
-		  gamestate = "game"
-		end
-	  elseif gamestate == "game" then
-		if key == "return" then
-		  gamestate = "roll"
-		end
-	  
-		
-		
-		local nextX = p1_x
-		local nextY = p1_y
-
-		if key == "w" and p1_y > 1 then
-			nextY = nextY - 1
-		elseif key == "s" and p1_y < 11 then
-			nextY = nextY + 1 -- other keys!
-		elseif key == "a" and p1_x > 1 then
-			nextX = nextX - 1 -- other keys!
-		elseif key == "d" and p1_x < 20 then
-			nextX = nextX + 1 -- other keys!	
-		end
-
-		if tilemap[nextY][nextX] == 2 or tilemap[nextY][nextX] == 3 then -- not a wall, move the player
-			p1_x = nextX
-			p1_y = nextY
-			fow()
-
-			if tilemap[p1_y][p1_x] == 3 then --finish!
-				print("Player 1 - You win!")
-			end
-		end
-
-		local p2next_x = p2_x
-		local p2next_y = p2_y
-
-		if key == "left" and p2_x > 1 then
-			p2next_x = p2next_x - 1
-		elseif key == "right" and p2_x < 20 then
-			p2next_x = p2next_x + 1
-			print(p2_x, p2_y)
-		elseif key == "up" and p2_y > 1 then
-			p2next_y = p2next_y - 1
-			print(p2_x, p2_y)
-		elseif key == "down" and p2_y < 11 then
-			p2next_y = p2next_y + 1
-			print(p2_x, p2_y)
-		end
-
-		if tilemap[p2next_y][p2next_x] == 1 or tilemap[p2next_y][p2next_x] == 3 then -- not a wall, move the player
-			p2_x = p2next_x
-			p2_y = p2next_y
-			fow()
-
-			if tilemap[p2_y][p2_x] == 3 then --finish!
-				print("Player 2 - You win!")
-			end
-		end
-	end
+	Gamestate.keypressed(key)
 end
 
 
@@ -158,70 +236,5 @@ end
 
 
 function love.draw()
-	if gamestate == "menu" then
-		love.graphics.push("all")
-		love.graphics.setColor(0, 0, 0)
-		love.graphics.printf("NEGATIVE SPACES", 0, 400, scr_width, "center")
-		love.graphics.printf( "PRESS START", 0, 500, scr_width, "center")
-		love.graphics.pop()
-
-		love.graphics.scale(4, 4)
-		for i,row in ipairs(tilemap) do  
-			for j,tile in ipairs(row) do 
-					local v = tilemap[i][j] + 1
-					if fogofwar[i][j] == 1 then
-						love.graphics.draw(tileset, quads[v], j * width, i * height)
-					end
-			end
-		end
-
-		
-		love.graphics.draw(white, player_width * p1_x, player_height * p1_y)
-		love.graphics.draw(black, player_width * p2_x, player_height * p2_y)
-	end
-
-	if gamestate == "game" then
-			love.graphics.push("all")
-			love.graphics.setColor(0, 0, 0)
-
-			love.graphics.printf("PLAYER " .. current_player .. " - YOUR TURN", 0, 400, scr_width, "center")
-			love.graphics.printf("PRESS ENTER TO ROLL A DICE", 0, 430, scr_width, "center")
-			love.graphics.pop()
-	
-			love.graphics.scale(4, 4)
-			for i,row in ipairs(tilemap) do  
-				for j,tile in ipairs(row) do 
-						local v = tilemap[i][j] + 1
-						if fogofwar[i][j] == 1 then
-							love.graphics.draw(tileset, quads[v], j * width, i * height)
-						end
-				end
-			end
-	
-			
-			love.graphics.draw(white, player_width * p1_x, player_height * p1_y)
-			love.graphics.draw(black, player_width * p2_x, player_height * p2_y)
-	end
-
-	if gamestate == "roll" then 
-		love.graphics.push("all")
-			love.graphics.setColor(0, 0, 0)
-
-			love.graphics.printf("ROLL A DICE!", 0, 400, scr_width, "center")
-			love.graphics.pop()
-	
-			love.graphics.scale(4, 4)
-			for i,row in ipairs(tilemap) do  
-				for j,tile in ipairs(row) do 
-						local v = tilemap[i][j] + 1
-						if fogofwar[i][j] == 1 then
-							love.graphics.draw(tileset, quads[v], j * width, i * height)
-						end
-				end
-			end
-	
-			
-			love.graphics.draw(white, player_width * p1_x, player_height * p1_y)
-			love.graphics.draw(black, player_width * p2_x, player_height * p2_y)
-		end
+	Gamestate:draw()
 end
